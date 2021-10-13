f5c0f594b93aec06ba2878ab765843bdc5e6b7d4
dedobbin <you@example.com>
2021-10-13 21:16:19 +0200

    properly process second param (limit) from tumblr.get_image_from_profile

--- tumblr.py
+++ tumblr.py
@@ -5,14 +5,13 @@ import cv2
 
 class tumblr:
     @staticmethod
-    def get_image_from_profile(profile:str, n: int):
+    def get_image_from_profile(profile:str, n: int = 0):
         profile_html: str = requests.get("http://www.%s.tumblr.com" % profile).text
         soup = BeautifulSoup(profile_html, 'html.parser')
         img_elements = soup.select('[src*="64.media.tumblr.com"]')
-        img_elements = img_elements[:]
+        img_elements = img_elements if n == 0 else img_elements[:n]
         imgs = list(map(lambda x: url_to_img(x["src"]), (img_elements)))
         return imgs
-
 
 def url_to_img(url):
     raw = requests.get(url, stream=True).raw
